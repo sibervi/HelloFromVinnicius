8df82681016528a3aa42bbb44344edd6bd32c05c
Álisson Inacio <ina21001@byui.edu>
2022-01-29 19:33:00 -0700

    Vinnicius code modified successfully

--- src/castro/Main.java
+++ src/castro/Main.java
@@ -9,5 +9,8 @@ public class Main {
         System.out.println("My favorite food is lasagna, but anything my mom make is Heaven to me");
 
         // Please tell your name, age, and favorite food
+
+        System.out.println("\nMy name is Alisson, I'm 25 years old and my favorite food is Churrasco");
+        System.out.println("Hello Norman and Vinnicius.");
     }
 }
